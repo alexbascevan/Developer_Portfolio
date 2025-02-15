942a9805099de052bd5ecb7d3f3d76b0d2841bc8
Alex Bascevan <alexbascevan@icloud.com>
2025-02-15 11:52:36 -0500

    Added icons to skills component, fixed styling

--- developer-portfolio/src/components/Skills/Skills.jsx
+++ developer-portfolio/src/components/Skills/Skills.jsx
@@ -11,66 +11,71 @@ function Skills() {
             <div className="leftblocks">
                 <div className="frontend">
                     <h2 className='skilldivheading'> Frontend</h2>
-                    <p className="skilldivdesc">Experienced in penetration testing, Linux security, and various cybersecurity practices, with a focus on identifying, exploiting and remediating vulnerabilities.</p>
+                    <p className="skilldivdesc">I'm passionate about design, animation and interactions, always aiming to build fun and responsive websites with great user experiences. </p>
                     <div class="tech-icons">
-                        <div><img src="/icons/kali.svg" alt="Kali Linux" class="svg-icon"/></div>
-                        <div><img src="/icons/htb.svg" alt="Hack The Box" class="svg-icon"/></div>
-                        <div><img src="/icons/metasploit.svg" alt="Metasploit" class="svg-icon"/></div>
-                        <div> <img src="/icons/ad.svg" alt="Active Directory" class="svg-icon"/></div>
-                        <div><img src="/icons/nmap.svg" alt="nmap" class="svg-icon"/></div>
-                        <div><img src="/icons/wireshark.svg" alt="wireshark"class="svg-icon"/></div>
-                        <div><img src="/icons/hydra.svg" alt="hydra" class="svg-icon"/></div>
-                        <div><img src="/icons/burpsuite.svg" alt="burpsuite" class="svg-icon"/></div>
+                        <div class="icon" data-label="React"><span class="iconify" data-icon="logos:react"></span></div>
+                        <div class="icon" data-label="Angular"><img src="/icons/angular.svg" alt="Angular" class="svg-icon"/></div>
+                        <div class="icon" data-label="JavaScript"><span class="iconify" data-icon="logos:javascript"></span></div>
+                        <div class="icon" data-label="TypeScript"><img src="/icons/typescript.svg" alt="TypeScript" class="svg-icon"/></div>
+                        <div class="icon" data-label="HTML5"><span class="iconify" data-icon="logos:html-5"></span></div>
+                        <div class="icon" data-label="CSS3"><span class="iconify" data-icon="logos:css-3"></span></div>
+                        <div class="icon" data-label="Sass"><span class="iconify" data-icon="logos:sass"></span></div>
+                        <div class="icon" data-label="Bootstrap"><span class="iconify" data-icon="logos:bootstrap"></span></div>
+                        <div class="icon" data-label="Kotlin"><img src="/icons/kotlin.svg" alt="Kotlin" class="svg-icon"/></div>
+                        <div class="icon" data-label="SwiftUI"><img src="/icons/swift.svg" alt="SwiftUI" class="svg-icon"/></div>
                     </div>
                 </div>
 
                 <div className="backend">
                     <h2 className='skilldivheading'> Backend</h2>
-                    <p className="skilldivdesc">I have managed AWS, GCP and various other cloud services during my education and my own projects.</p>
+                    <p className="skilldivdesc">I love problem solving and building complex systems. I strive to create scalable, reliable and secure software solutions.</p>
                     <div class="tech-icons">
-                        <div class="icon"><span class="iconify" data-icon="logos:aws"></span></div> 
-                        <div class="icon"><span class="iconify" data-icon="logos:google"></span></div>
-                        <div class="icon"><span class="iconify" data-icon="logos:linux-tux"></span></div>
-                        <div class="icon"><span class="iconify" data-icon="logos:docker-icon"></span></div>
-                        <div class="icon"><span class="iconify" data-icon="logos:jenkins"></span></div> 
-                        <div class="icon"><span class="iconify" data-icon="logos:github-icon"></span></div>
-                        <div class="icon"><span class="iconify" data-icon="logos:gitlab"></span></div> 
+                        <div class="icon" data-label="Spring"><img src="/icons/spring.svg" alt="Spring" class="svg-icon"/></div>
+                        <div class="icon" data-label="Java"><img src="/icons/java.svg" alt="Java" class="svg-icon"/></div>
+                        <div class="icon" data-label="ASP.NET"><img src="/icons/net.svg" alt="ASP.NET" class="svg-icon"/></div>
+                        <div class="icon" data-label="PHP"><img src="/icons/php.svg" alt="PHP" class="svg-icon"/></div>
+                        <div class="icon" data-label="Oracle DB"><img src="/icons/oracle.svg" alt="Oracle DB" class="svg-icon"/></div>
+                        <div class="icon" data-label="MongoDB"><img src="/icons/mongo.svg" alt="MongoDB" class="svg-icon"/></div>
+                        <div class="icon" data-label="cPanel"><img src="/icons/cpanel.svg" alt="cPanel" class="svg-icon"/></div>
+                        <div class="icon" data-label="Tomcat"><img src="/icons/tomcat.svg" alt="Tomcat" class="svg-icon"/></div>
+                        <div class="icon" data-label="Postman"><img src="/icons/postman.svg" alt="Postman" class="svg-icon"/></div>
                     </div>
                 </div>
             </div>
 
 
             <div className="rightblocks">
-                <div className="cybersecurity">
-                    <h2 className='skilldivheading'> Cybersecurity</h2>
-                    <p className="skilldivdesc">Experienced in penetration testing, Linux security, and various cybersecurity practices, with a focus on identifying, exploiting and remediating vulnerabilities.</p>
+                <div className="cloud">
+                    <h2 className='skilldivheading'> Cloud & DevOps</h2>
+                    <p className="skilldivdesc">I am experienced in managing cloud services including AWS, GCP through academic projects and personal initiatives, with a strong understanding of DevOps practices and Agile methodologies.</p>
                     <div class="tech-icons">
-                        <div><img src="/icons/kali.svg" alt="Kali Linux" class="svg-icon"/></div>
-                        <div><img src="/icons/htb.svg" alt="Hack The Box" class="svg-icon"/></div>
-                        <div><img src="/icons/metasploit.svg" alt="Metasploit" class="svg-icon"/></div>
-                        <div> <img src="/icons/ad.svg" alt="Active Directory" class="svg-icon"/></div>
-                        <div><img src="/icons/nmap.svg" alt="nmap" class="svg-icon"/></div>
-                        <div><img src="/icons/wireshark.svg" alt="wireshark"class="svg-icon"/></div>
-                        <div><img src="/icons/hydra.svg" alt="hydra" class="svg-icon"/></div>
-                        <div><img src="/icons/burpsuite.svg" alt="burpsuite" class="svg-icon"/></div>
+                        <div class="icon" data-label="AWS"><span class="iconify" data-icon="logos:aws"></span></div> 
+                        <div class="icon" data-label="Google Cloud"><span class="iconify" data-icon="logos:google"></span></div>
+                        <div class="icon" data-label="Linux"><span class="iconify" data-icon="logos:linux-tux"></span></div>
+                        <div class="icon" data-label="Docker"><span class="iconify" data-icon="logos:docker-icon"></span></div>
+                        <div class="icon" data-label="Jenkins"><span class="iconify" data-icon="logos:jenkins"></span></div> 
+                        <div class="icon" data-label="Ansible"><img src="/icons/ansible.svg" alt="Ansible" class="svg-icon"/></div>
+                        <div class="icon" data-label="GitHub"><span class="iconify" data-icon="logos:github-icon"></span></div>
+                        <div class="icon" data-label="GitLab"><span class="iconify" data-icon="logos:gitlab"></span></div>
                     </div>
                 </div>
 
-                <div className="cloud">
-                    <h2 className='skilldivheading'> Cloud & DevOps</h2>
-                    <p className="skilldivdesc">I have managed AWS, GCP and various other cloud services during my education and my own projects.</p>
+                <div className="cybersecurity">
+                    <h2 className='skilldivheading'> Cybersecurity</h2>
+                    <p className="skilldivdesc">I have experience in penetration testing, Linux security, and various cybersecurity practices, with a strong focus on identifying, exploiting, and remediating vulnerabilities.</p>
                     <div class="tech-icons">
-                        <div class="icon"><span class="iconify" data-icon="logos:aws"></span></div> 
-                        <div class="icon"><span class="iconify" data-icon="logos:google"></span></div>
-                        <div class="icon"><span class="iconify" data-icon="logos:linux-tux"></span></div>
-                        <div class="icon"><span class="iconify" data-icon="logos:docker-icon"></span></div>
-                        <div class="icon"><span class="iconify" data-icon="logos:jenkins"></span></div> 
-                        <div class="icon"><span class="iconify" data-icon="logos:github-icon"></span></div>
-                        <div class="icon"><span class="iconify" data-icon="logos:gitlab"></span></div> 
+                        <div class="icon" data-label="Kali"><img src="/icons/kali.svg" alt="Kali Linux" class="svg-icon"/></div>
+                        <div class="icon" data-label="Hack The Box"><img src="/icons/htb.svg" alt="Hack The Box" class="svg-icon"/></div>
+                        <div class="icon" data-label="Metasploit"><img src="/icons/metasploit.svg" alt="Metasploit" class="svg-icon"/></div>
+                        <div class="icon" data-label="Active Directory"><img src="/icons/ad.svg" alt="Active Directory" class="svg-icon"/></div>
+                        <div class="icon" data-label="Nmap"><img src="/icons/nmap.svg" alt="Nmap" class="svg-icon"/></div>
+                        <div class="icon" data-label="Wireshark"><img src="/icons/wireshark.svg" alt="Wireshark" class="svg-icon"/></div>
+                        <div class="icon" data-label="Hydra"><img src="/icons/hydra.svg" alt="Hydra" class="svg-icon"/></div>
+                        <div class="icon" data-label="Burp Suite"><img src="/icons/burpsuite.svg" alt="Burp Suite" class="svg-icon"/></div>
+                        <div class="icon" data-label="Bash"><img src="/icons/bash.svg" alt="Bash" class="svg-icon"/></div>
                     </div>
                 </div>
             </div>
-
         </div>
 
     </section>
