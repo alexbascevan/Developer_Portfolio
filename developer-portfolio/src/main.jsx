638b6a4c01b55b863aecf167d0867f647fe62882
Alex Bascevan <alexbascevan@icloud.com>
2025-02-14 22:44:31 -0500

    Remove files already tracked by Git and add .gitignore

---
+++ developer-portfolio/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+
+import CustomCursor from './components/CustomCursor/CustomCursor'
+import Home from './components/home/Home'
+import Skills from './components/Skills/Skills'
+import AboutMe from './components/AboutMe/AboutMe'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <CustomCursor/>  disable mouse cursor in body index.css */}
+
+    <div>
+      <Home />
+      <hr/>
+      <AboutMe/>
+      <hr/>
+      <Skills/>
+    </div>
+  </StrictMode>,
+)
