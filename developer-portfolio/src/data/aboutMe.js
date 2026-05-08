e00379f0257c7358344efe8ab5707c027bcb3aa3
alexbascevan <alexbascevan@icloud.com>
2026-05-07 22:47:36 -0400

    Rearranged education in about me

--- developer-portfolio/src/data/aboutMe.js
+++ developer-portfolio/src/data/aboutMe.js
@@ -6,20 +6,21 @@ export const aboutMeData = {
       className: "contentBlock",
       heading: "Education",
       entries: [
+         {
+          title: "HONOURS BACHELOR OF INFORMATION SCIENCES (CYBERSECURITY) ",
+          subtitle: "Sheridan College",
+          date: "May 2026 - PRESENT",
+          description:
+            "Continuing academic progression into the cybersecurity discipline through Sheridan’s Honours Bachelor of Cybersecurity program. Expanding expertise in penetration testing, digital forensics, secure architecture, governance, threat intelligence, and enterprise security operations."
+        },
         {
           title: "SOFTWARE DEVELOPMENT & NETWORK ENGINEERING",
           subtitle: "ASSOCIATE'S DEGREE | Sheridan College",
           date: "January 2023 - December 2025",
           description:
             "Completed an Associate’s degree with a focus on software engineering, backend development, networking fundamentals, and real-world application design. Developed hands-on experience through collaborative projects and a competitive capstone, with emphasis on system architecture, APIs, databases, and cloud deployment."
-        },
-        {
-          title: "HONOURS BACHELOR OF INFORMATION SCIENCES (CYBERSECURITY) ",
-          subtitle: "Sheridan College",
-          date: "May 2026 - PRESENT",
-          description:
-            "Continuing academic progression into the cybersecurity discipline through Sheridan’s Honours Bachelor of Cybersecurity program. Expanding expertise in penetration testing, digital forensics, secure architecture, governance, threat intelligence, and enterprise security operations."
         }
+       
       ]
     },
     {
