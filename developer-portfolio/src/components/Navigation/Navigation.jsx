0b84ee9121044acfbdbb3293cf8d76026057b104
alexbascevan <alexbascevan@icloud.com>
2026-05-11 20:43:15 -0400

    Relocated navigation.css to styles folder

--- developer-portfolio/src/components/Navigation/Navigation.jsx
+++ developer-portfolio/src/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from '../../hooks/useTheme';
-import './navigation.css';
+import '../../styles/navigation.css';
 
 const Navigation = () => {
   const [activeSection, setActiveSection] = useState('home');
